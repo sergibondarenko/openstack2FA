40bd26c3e292696b7f885729831e296cfb5827b0
Marco Pesenti <badmarkdevil@gmail.com>
2016-03-03 14:36:34 +0100

    New user message: Table 'fastcloud.horizon2fa_user' doesn't exist

--- horizon2fa/urls.py
+++ horizon2fa/urls.py
@@ -18,6 +18,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index$', views.index, name='index'),
     url(r'^login$', views.loginview, name='loginview'),
-    url(r'^new$', views.newview, name='newview'),
+    url(r'^newview$', views.newview, name='newview'),
+    url(r'^code$', views.code, name='code'),
+    url(r'^new$', views.new, name='new'),
+    url(r'^qr.*', views.qr, name='qr'),
+    url(r'^otpconfirm$', views.otpconfirm, name='otpconfirm'),
 ]
 
