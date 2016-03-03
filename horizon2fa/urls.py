10a11430e8cca4661e4880add6f211066f97371e
Marco Pesenti <badmarkdevil@gmail.com>
2016-03-03 18:48:27 +0100

    Application working, style and test pending.

--- horizon2fa/urls.py
+++ horizon2fa/urls.py
@@ -17,10 +17,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index$', views.index, name='index'),
-    url(r'^login$', views.loginview, name='loginview'),
+    url(r'^loginview$', views.loginview, name='loginview'),
+    url(r'^login$', views.login, name='login'),
     url(r'^newview$', views.newview, name='newview'),
-    url(r'^code$', views.code, name='code'),
     url(r'^new$', views.new, name='new'),
+    url(r'^code$', views.code, name='code'),
     url(r'^qr.*', views.qr, name='qr'),
     url(r'^otpconfirm$', views.otpconfirm, name='otpconfirm'),
 ]
